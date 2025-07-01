005186f6e3bea33e7944487e620af5b822fc261c
Alexandre <32963519+alexand7e@users.noreply.github.com>
2025-07-01 16:19:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from datetime import datetime
 from queue import Queue
 import threading
 
-from moviepy.editor import VideoFileClip
+from moviepy.video.io.VideoFileClip import VideoFileClip
 from pydub import AudioSegment
 import torch
 import whisper
